689d360d2124bf5a4eef36a6957c025e30132795
BilPinheiro <prod.gabriel.pinheiro@gmail.com>
2022-08-24 20:19:10 -0300

    Cadastros

---
+++ cadastroFuncionariosECategorias.sql
@@ -0,0 +1,12 @@
+insert into categoria (cat_tx_nome, cat_tx_descricao)
+values 
+('Action Figures', 'Um action-figure é um figura plástica de um personagem que pode mudar de posição, ou seja, pode mudar sua posição de ação, elas são frequentemente de filmes, animes, de Video-games, ou de programas de televisão. Diferente de estátuas, que são fixas e não possuem movimento.'),
+('Cards colecionáveis', 'Os cards são pedaços interativos de informação apresentados quase sempre num formato retangular. Assim como cartões de desenhos ou cartas de jogos, os cards da web contém informações resumidas, relevantes e de rápida compreensão.'),
+('Jogos de Tabuleiro', 'Os jogos de tabuleiro, utilizam superfícies, normalmente planas, pré-determinadas, com marcações de acordo com as regras de cada jogo. Desenvolvidos para serem jogados por um ou mais participantes, os jogos de tabuleiro podem demandar estratégia, sorte, conhecimento ou memória.')
+insert into funcionario (fun_tx_nome, fun_tx_cpf, fun_tx_cep, fun_tx_rua, fun_int_numero, fun_tx_telefone, fun_tx_email)
+values
+('Gabriel Pinheiro', '12378945600', '28500-123', 'Rua do Popeie', 1203, '32550945', 'Ograndemarinheiropopeie@gmail.com'),
+('Anthony barbosa', '13588429600', '286600-000', 'Rua do Popeie', 04, '25452345', 'gokucareca15@gmail.com'),
+('Nathan Dias', '19020071742', '28620-000', 'Rua dos Bobos ', 0, '25215324', 'casamuitoengraçada@gmail.com'),
+('Barbara Souza', '06372448777', '25961-265', 'Rua Jorge Lossio', 500, '32550945', 'barbarasouza@gmail.com'),
+('Rivania Gomes', '19521064727', '25960-000', 'Rua dos Trouxas ', 240, '99673256', 'vaninhacat123@hotmail.com')
