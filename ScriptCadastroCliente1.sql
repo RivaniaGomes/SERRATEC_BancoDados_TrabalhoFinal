5a1f4979ebeda94fb387bd7aa4af4b00bfafbda5
Nathan Dias <nathanfaria9@gmail.com>
2022-08-24 22:41:12 -0300

    cinco clientes

--- ScriptCadastroCliente1.sql
+++ ScriptCadastroCliente1.sql
@@ -1,4 +1,8 @@
 INSERT INTO public.cliente
 (cli_tx_nome_completo, cli_tx_nome_de_usuario, cli_tx_email, cli_tx_cpf, cli_dt_data_de_nascimento, cli_tx_cep, cli_tx_rua, cli_int_numero, cli_tx_telefone)
 VALUES('Giussep Mario Verone', 'Mario Bros', 'marinhobros@gmail.com', '200.190.747-23', '1981-05-21', '28620-300', 'Rua Magnólios', 13, '2324-2324'),
-('Luis Lambari Vasconselos', 'Luis Lambari', 'lui2324@gmail.com', '105.390.707-11', '1951-05-21', '28620-300', 'Rua Magnólios', 13, '2324-2324'),
+('Luis Lambari Vasconselos', 'Luis Lambari', 'lui2324@gmail.com', '105.390.707-11', '1951-05-01', '55720-220', 'Rua Salvatore', 24, '3324-2399'),
+('Leonardo Marquez Rocha', 'Leonardo Marquez', 'leo44marq@gmail.com', '999.890.756-22', '2001-07-11', '98271-220', 'Rua Einstein', 14, '1114-8890'),
+('Alfredo Alves Aleman', 'Alfredo Alvez', 'alfred42pai@gmail.com', '222.092.123-23', '1999-11-27', '97842-121', 'Rua Lorena', 7, '1246-7271'),
+('Breno Macário Fernandes', 'Breno Mac', 'brenomaccpt@gmail.com', '190.200.717-42', '2002-04-01', '24500-123', 'Rua Marilia', 741, '2523-7767'),
+ 
