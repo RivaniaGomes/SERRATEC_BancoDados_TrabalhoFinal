cda8e77d621f44814f889a1ee9c779af34203f44
Nathan Dias <nathanfaria9@gmail.com>
2022-08-24 22:29:28 -0300

    primeiro cliente

---
+++ ScriptCadastroCliente1.sql
@@ -0,0 +1,4 @@
+INSERT INTO public.cliente
+(cli_tx_nome_completo, cli_tx_nome_de_usuario, cli_tx_email, cli_tx_cpf, cli_dt_data_de_nascimento, cli_tx_cep, cli_tx_rua, cli_int_numero, cli_tx_telefone)
+VALUES('Giussep Mario Verone', 'Mario Bros', 'marinhobros@gmail.com', '200.190.747-23', '1981-05-21', '28620-300', 'Rua Magnólios', 13, '2324-2324'),
+('Luis Lambari Vasconselos', 'Luis Lambari', 'lui2324@gmail.com', '105.390.707-11', '1951-05-21', '28620-300', 'Rua Magnólios', 13, '2324-2324'),
