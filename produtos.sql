718edf28c352ae18c35001dc16affedad33d10eb
BilPinheiro <prod.gabriel.pinheiro@gmail.com>
2022-08-24 23:07:12 -0300

    Lista produtos

---
+++ produtos.sql
@@ -0,0 +1,9 @@
+insert into produto(prd_tx_nome, prd_tx_descricao, prd_int_quantidade_estoque, prd_dt_data_de_fabricacao, prd_dec_valor_unitario, fun_cd_id, cat_cd_id)
+values 
+('Dragon Ball Z - Blood Of Saiyajins - Goku Super Saiyajin', 'Mais uma figura da coleção blood of saiyajins desta vez com goku em sua versão super saiyajin em figura de 20 cm.', 5,  '2019-1-1', 399.90, 13, 1),
+('Booster - A Guerra da Centelha / Magic The Gathering', 'Booster avulso - A Guerra da Centelha / War of the Spark - Contém 15 cartas', 40,  '2022-05-20', 29.90, 13, 2),
+('Booster - Alara Reunida / Magic The Gathering', 'Booster avulso - Alara Reborn / Alara Reunida - Contém 15 cartas', 40,  '2022-05-20', 174.90, 13, 2),
+('Starter Deck Batalha V Pokémon GO Melmetal', 'Melmetal V dá a si mesmo um aumento de Energia e, em seguida, golpeia o seu oponente causando um dano enorme! Peça reforços de atacantes fortes, como Cobalion e Unfezant, coloque mais Energia em jogo com Perrserker de Galar e tenha ajuda de Magearna para comprar mãos novas.', 5,  '2021-10-10', 59.90, 13, 2),
+('Booster - Espada e Escudo / Pokémon TCG', 'Cada pacote de booster do Pokémon Estampas Ilustradas contém 6 cartas no formato 63mm x 88mm. ', 40,  '2020-03-16', 9.90, 13, 2),
+('Deck de Commander - Dominária Unida - Legado das Lendas (WBR)', '100 cartas prontas para jogar, Pacote de Exemplo de Booster de Colecionador com 2 cards.', 5,  '2022-06-05', 303.90, 13, 2);
+
