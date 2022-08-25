c13c900ad71671f2a8a66c82de4252c3ca633095
Bárbara Souza <barbarasoouzza@gmail.com>
2022-08-24 23:03:08 -0300

    Finalizando Clientes

--- ScriptCadastroCliente1.sql
+++ ScriptCadastroCliente1.sql
@@ -5,4 +5,11 @@ VALUES('Giussep Mario Verone', 'Mario Bros', 'marinhobros@gmail.com', '200.190.7
 ('Leonardo Marquez Rocha', 'Leonardo Marquez', 'leo44marq@gmail.com', '999.890.756-22', '2001-07-11', '98271-220', 'Rua Einstein', 14, '1114-8890'),
 ('Alfredo Alves Aleman', 'Alfredo Alvez', 'alfred42pai@gmail.com', '222.092.123-23', '1999-11-27', '97842-121', 'Rua Lorena', 7, '1246-7271'),
 ('Breno Macário Fernandes', 'Breno Mac', 'brenomaccpt@gmail.com', '190.200.717-42', '2002-04-01', '24500-123', 'Rua Marilia', 741, '2523-7767'),
+('Felipe Martins Rocha', 'Felipe Martins', 'felipemartins@gmail.com', '222.333.444-55', '1995-05-13', '22369-258', 'Avenida Fonseca' , 500, '3535-6666');
+('Joana Ribeiro', 'Joana Ribeiro', 'joanaribeiro@gmail.com', '369.258.147-55', '2001-08-12', '25471-963', 'Rua Teixeira' , 357, '3642-5555');
+('Richard Oliveira Gomes', 'Richard Gomes', 'richardgomes@gmail.com', '741.852.963-33', '1998-06-09', '25852-963', 'Avenida Oliveira' , 430, '2641-8888');
+('Daniel de Souza', 'Daniel Souza', 'danielsouza@gmail.com', '521.468.796-99', '2004-02-26', '25874-965', 'Avenida Rio Branco' , 123, '2642-1030');
+('Larissa Soares Fagundes', 'Larissa Fagundes', 'larissafagundes@gmail.com', '258.456.852-77', '1994-06-09', '25893-456', 'Avenida Goias' , 258, '3535-3333');
+('Nathalia Rocha Ferreira', 'Nathalia Rocha', 'nathaliarocha@gmail.com', '456.267.159.88', '2002-07-16', '25698-456', 'Rua Seis' , 77, '2642-7979');
+
  
