145589481c5db607dd1bafd06e842351a27d03e4
Rivânia Gomes <rivaniagomes@gmail.com>
2022-08-26 11:16:05 -0300

    Adição do ER e MER feitos no LucidChart

--- ScriptTesteRivania.sql
+++ ScriptTesteRivania.sql
@@ -16,19 +16,22 @@ select cli_tx_cpf as cpf,
 cli_tx_nome_de_usuario as cliente,
 pedido_item.ped_cd_id as nr_pedido,
 ped_dt_data_pedido as data_pedido,
-pdt_int_quantidade as quantidade,
 cat_tx_nome as categoria,
 prd_tx_nome as nome_produto,
+pdt_int_quantidade as quantidade,
 prd_dec_valor_unitario as valor_produto,
 prd_dec_valor_unitario * pdt_int_quantidade as valor_total
 from pedido
-join pedido_item
+inner join pedido_item
 on pedido.ped_cd_id = pedido_item.ped_cd_id
-join produto
+inner join produto
 on produto.prd_cd_id = pedido_item.prd_cd_id
-join categoria
+inner join categoria
 on categoria.cat_cd_id = produto.cat_cd_id
-join cliente
+inner join cliente
 on cliente.cli_cd_id = pedido.cli_cd_id;
+
+select 
+
 
 
