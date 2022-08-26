45d5509469cd291370ee57c52a4d3f77042fecdb
Rivânia Gomes <rivaniagomes@gmail.com>
2022-08-26 17:53:48 -0300

    Alteração script

--- 7ScriptsParaOSucesso.sql
+++ 7ScriptsParaOSucesso.sql
@@ -1,8 +1,69 @@
+--Relatorio cadastro de clientes (View).
+select
+cli_cd_id as cod_cliente,
+cli_tx_cpf as cpf,
+cli_tx_nome_completo as nome_completo,
+cli_tx_nome_de_usuario as usuario,
+cli_tx_email as email,
+cli_tx_rua || ', ' || cli_int_numero || ', ' || cli_tx_cep
+as endereco
+from cliente;
+
+--Relatorio Produto, Categoria e Funcionario responsavel (View)
+select 
+prd_tx_nome as produto, 
+cat_tx_nome as categoria,
+fun_tx_nome as funcionario,
+produto.prd_int_quantidade_estoque as estoque
+from produto 
+inner join funcionario
+on produto.fun_cd_id = funcionario.fun_cd_id
+inner join categoria
+on categoria.cat_cd_id = produto.cat_cd_id;
+
+--Relatorio Pedidos de Venda (View).
+select 
+cli_tx_nome_de_usuario as usuario,
+pedido_item.ped_cd_id as nr_pedido,
+ped_dt_data_pedido as data_pedido,
+pdt_int_quantidade as quantidade,
+cat_tx_nome as categoria,
+prd_tx_nome as nome_produto,
+prd_dec_valor_unitario as valor_produto,
+prd_dec_valor_unitario * pdt_int_quantidade as valor_total
+from
+	pedido
+inner join pedido_item
+on pedido.ped_cd_id = pedido_item.ped_cd_id
+inner join produto
+on produto.prd_cd_id = pedido_item.prd_cd_id
+inner join categoria
+on categoria.cat_cd_id = produto.cat_cd_id
+inner join cliente
+on cliente.cli_cd_id = pedido.cli_cd_id;
+
+--Relatorio produtos pro categoria (view). 
+select
+c.cat_tx_nome as categoria,
+   count(p.cat_cd_id) as quantidade
+from categoria c
+join produto p
+on p.cat_cd_id = c.cat_cd_id  
+group by cat_tx_nome;
+
+--Atualização
+UPDATE public.produto
+SET prd_int_quantidade_estoque=40
+WHERE prd_cd_id= 12;
+
+--Deletar Tabela/Entidade
+drop table nota_fiscal;
+
 --Nota Fiscal somando todos os produtos.
 select 
 cli_tx_cpf as cpf,
 cli_tx_nome_de_usuario as cliente,
-pedido_item.ped_cd_id as nota__pedido,
+pedido_item.ped_cd_id as nota_pedido,
 ped_dt_data_pedido as data_pedido,
 pdt_int_quantidade as quantidade,
 cat_tx_nome as categoria,
@@ -12,13 +73,13 @@ prd_dec_valor_unitario * pdt_int_quantidade as valor_total,
 ( select sum(prd_dec_valor_unitario * pdt_int_quantidade)
 from
 	pedido
-join pedido_item
+inner join pedido_item
 on pedido.ped_cd_id = pedido_item.ped_cd_id
-join produto
+inner join produto
 on produto.prd_cd_id = pedido_item.prd_cd_id
-join categoria
+inner join categoria
 on categoria.cat_cd_id = produto.cat_cd_id
-join cliente
+inner join cliente
 on cliente.cli_cd_id = pedido.cli_cd_id
 where 
 	pedido_item.ped_cd_id = :limite
@@ -26,37 +87,14 @@ group by
 	pedido_item.ped_cd_id
 ) as valor_total_nota
 from pedido
-join pedido_item
+inner join pedido_item
 on pedido.ped_cd_id = pedido_item.ped_cd_id
-join  produto
+inner join  produto
 on produto.prd_cd_id = pedido_item.prd_cd_id
-join categoria
+inner join categoria
 on categoria.cat_cd_id = produto.cat_cd_id
-join cliente
+inner join cliente
 on cliente.cli_cd_id = pedido.cli_cd_id
 where pedido.ped_cd_id = :limite;
 
---Atualização
 
-UPDATE public.produto
-SET prd_int_quantidade_estoque=40
-WHERE prd_cd_id= 12;
-
--- Deletar Tabela/Entidade
-
-drop table nota_fiscal;
-
--- Script count() contando a variedade de produtos de Cards Colecionaveis. Tem na loja. Categoria 2. 
-
-select count(cat_cd_id), cat_cd_id  from produto group by cat_cd_id;
-
--- Produto, Categoria e Funcionario responsavel()
-select 
-prd_tx_nome as produto, 
-cat_tx_nome as categoria,
-fun_tx_nome as funcionario
-from produto 
-inner join funcionario
-on produto.fun_cd_id = funcionario.fun_cd_id
-inner join categoria
-on categoria.cat_cd_id = produto.cat_cd_id;
